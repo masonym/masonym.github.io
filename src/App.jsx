fb356fac930f4b2eb34a7dfef4b39cac9b2cb377
Mason Leitch <nosamleitch@gmail.com>
2024-07-22 19:13:11 -0700

    Initial footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import Header from './Header.jsx'
+import Footer from './Footer.jsx'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Header/>
-  )
+    <>
+      <Header/>
+      <Footer/>
+    </>
+  );
 }
 
 export default App
---
+++ src/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+
+    return(
+        // this should be column not row
+        <footer>
+            <p>made by mason leitch?</p>
+            <ul>
+                <li>placeholder github</li>
+                <li>placeholder linkedin</li>
+                <li>placeholder email</li>
+            </ul>
+        </footer>
+
+    )
+}
+
+export default Footer
