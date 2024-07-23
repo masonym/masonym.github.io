390f75312b5f9cd453e369584ee1cc744e254217
Mason Leitch <nosamleitch@gmail.com>
2024-07-22 18:57:06 -0700

    Initial header component

---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+
+    return(
+        <header>
+            <h1>Hello!</h1>
+            <nav>
+                <ul>
+                    <li><a href="">resume</a></li>
+                    <li><a href="">linkedin</a></li>
+                    <li><a href="">github</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+
+}
+
+export default Header
