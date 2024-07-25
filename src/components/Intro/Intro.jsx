229c120f73432e668a6a1b1757514cde577af4eb
Mason Leitch <nosamleitch@gmail.com>
2024-07-24 23:08:22 -0700

    Modified intro text

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -5,10 +5,20 @@ function Intro() {
 
 
     return (
-        <>
-            <h1>I'm <span style={{color: "#2063d6"}}>Mason</span>.</h1>
-            <h2>I'm a fresh graduate, I studied Computer Science at the University of the Fraser Valley</h2>
-        </>
+        <div className={styles.introWrapper}>
+            <h1 style={{ textAlign: "left", margin: 0 }}>Nice to meet you! 👋 <br></br>I'm <span style={{ color: "#2063d6" }}>Mason</span>.</h1>
+            <div className={styles.introParagraph}>
+                <p>
+                        I'm from the Greater Vancouver area, and eager to join a talented team building exciting products.
+                        <br></br><br></br>
+                        I have a keen interest lately in web development, and recently I've been building a few projects in React/Next.js. I love to learn, and ever since I finished my degree, that urge has been fueled like never before.
+
+                        <br></br><br></br>
+
+                        Currently learning: <b>C#</b><b></b>.
+                </p>
+            </div>
+        </div>
     );
 }
 
