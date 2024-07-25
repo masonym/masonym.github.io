a16072831b2b6834321f5548c719ec84059650f9
Mason Leitch <nosamleitch@gmail.com>
2024-07-25 00:14:52 -0700

    Added extra text to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,7 +9,10 @@ function Footer() {
     return(
         // this should be column not row
         <footer className={styles.footer}>
-            <p className={styles.footerText}>&copy; Mason Leitch {(new Date().getFullYear())}</p>
+            <div className={styles.footerText}>
+                <p >&copy; Mason Leitch {(new Date().getFullYear())}</p>
+                <p style={{marginTop: "-1em", fontSize: "0.75em"}}>Page built with React</p>
+            </div>
             <ul className={styles.socialLinks}>
                 <li><a href="https://github.com/masonym" target="_blank"><img src={gitHubLogo} className={styles.socialLogos}></img></a></li>
                 <li><a href="https://www.linkedin.com/in/mason-leitch-78b90a206/" target="_blank"><img src={linkedInLogo} className={styles.socialLogos}></img></a></li>
