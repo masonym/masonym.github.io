9e006d7e437099e318a79f0eaafdb79dfbc14303
Mason Leitch <nosamleitch@gmail.com>
2024-07-25 00:15:06 -0700

    Added portrait to intro

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,4 +1,5 @@
-import Greetings from '../Greetings/Greetings';
+import portrait from '../../assets/cropped.jpg'
+
 import styles from './Intro.module.css'
 
 function Intro() {
@@ -6,17 +7,20 @@ function Intro() {
 
     return (
         <div className={styles.introWrapper}>
-            <hr style={{color: 'white', width: '100%'}}/>
-            <h1 style={{ textAlign: "left", margin: 0 }}>Nice to meet you! 👋 <br></br>I'm <span style={{ color: "#2063d6" }}>Mason</span>.</h1>
+            <hr style={{ color: 'white', width: '100%' }} />
+            <div className={styles.greetingAndPortrait}>
+                <h1 style={{ textAlign: "left", margin: 0 }}>Nice to meet you! 👋 <br></br>I'm <span style={{ color: "#2063d6" }}>Mason</span>.</h1>
+                <img src={portrait} className={styles.portrait}></img>
+            </div>
             <div className={styles.introParagraph}>
                 <p>
-                        I'm from the Greater Vancouver area, and eager to join a talented team building exciting products.
-                        <br></br><br></br>
-                        I have a keen interest lately in web development, and recently I've been building a few projects in React/Next.js. I love to learn, and ever since I finished my degree, that urge has been fueled like never before.
+                    I'm from the Greater Vancouver area, and eager to join a talented team building exciting products.
+                    <br></br><br></br>
+                    I have a keen interest lately in web development, and recently I've been building a few projects in React/Next.js. I love to learn, and ever since I finished my degree, that urge has been fueled like never before.
 
-                        <br></br><br></br>
+                    <br></br><br></br>
 
-                        Currently learning: <b>C#</b><b></b>.
+                    Currently learning: <b>C#</b><b></b>.
                 </p>
             </div>
         </div>
