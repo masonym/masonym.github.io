a32947871ea3831d5e9cd16d563dda6e37c6d928
Mason Leitch <nosamleitch@gmail.com>
2024-07-26 10:59:17 -0700

    Added 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,38 @@
 import { useRef } from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from './components/Header/Header.jsx';
 import Greetings from './components/Greetings/Greetings.jsx';
-import Intro from './components/Intro/Intro.jsx'
+import Intro from './components/Intro/Intro.jsx';
 import Projects from './components/Projects/Projects.jsx';
 import Footer from './components/Footer/Footer.jsx';
-
-import styles from './App.module.css';
-
+import NotFound from './components/NotFound/NotFound.jsx';
 
 function App() {
   const homeRef = useRef(null);
   const projectsRef = useRef(null);
 
   return (
-    <>
-      <Header homeRef={homeRef} projectsRef={projectsRef} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Header homeRef={homeRef} projectsRef={projectsRef} />
+            
+            <div ref={homeRef}>
+              <Greetings />
+              <Intro />
+            </div>
       
-      <div ref={homeRef}>
-        <Greetings />
-        <Intro />
-      </div>
-
-      <div ref={projectsRef}>
-        <Projects/>
-      </div>
-      
-      <Footer />
-    </>
+            <div ref={projectsRef}>
+              <Projects/>
+            </div>
+            
+            <Footer />
+          </>
+        }/>
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
