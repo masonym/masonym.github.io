b60f46a148ea405e8eba143be19db416f48f893d
Mason Leitch <nosamleitch@gmail.com>
2024-09-06 11:26:09 -0700

    Added new projects & adjusted grid layout for project cards

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,6 +6,8 @@ import projectImageCS from '../../assets/upcoming-sales.png';
 import projectImagePhace from '../../assets/phace.png';
 import projectImageGB from '../../assets/chatbot_sampleImage.png';
 import projectImageDotNet from '../../assets/dotnetAPI_sampleImage.png';
+import projectImageMapleTools from '../../assets/maplestory-tools.png'
+import projectImageADA from '../../assets/ada.png'
 
 function Projects() {
     return (
@@ -14,11 +16,25 @@ function Projects() {
             <div className={styles.cardContainerWrapper}>
                 <div className={styles.cardContainer}>
                     <Card
+                        imageSource={projectImageMapleTools}
+                        name="MapleStory Tools Project"
+                        description="A scalable, React-based web application serving as a repository for various tools for an online game, designed with modular architecture to easily accommodate future game-related calculators and utilities."
+                        projectLink="https://masonym.dev"
+                        gitHubLink="https://github.com/masonym/masonym.dev"
+                    />
+                    <Card
                         imageSource={projectImageCS}
                         name="MapleStory Upcoming Sales Viewer"
                         description="A React website built in conjunction with Python XML parsing to display upcoming cash shop sales for the MMORPG MapleStory."
                         projectLink="https://masonym.dev/ms-upcoming-sales"
                         gitHubLink="https://github.com/masonym/maple-cs-parser"
+                    />
+                    <Card
+                        imageSource={projectImageADA}
+                        name="Military Contracting Event Management Website"
+                        description="A responsive event management website using Next.js and React, utilizing dynamic routing and centralized data files to generate SEO-friendly pages on-demand."
+                        projectLink="https://americandefensealliance.org/"
+                        gitHubLink="https://github.com/masonym/ada-website"
                     />
                     <Card
                         imageSource={projectImagePhace}
