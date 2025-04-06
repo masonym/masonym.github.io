6932c62845db4aaf49fbeca776e66656a4876d89
Mason Leitch <nosamleitch@gmail.com>
2025-04-06 04:00:55 -0700

    Feat: Updates to intro section & profile picture

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,4 +1,4 @@
-import portrait from '../../assets/cropped.jpg'
+import portrait from '../../assets/profile-portrait.jpg'
 
 import styles from './Intro.module.css'
 
@@ -16,11 +16,11 @@ function Intro() {
                 <p>
                     I'm from the Greater Vancouver area, and eager to join a talented team building exciting products.
                     <br></br><br></br>
-                    I have a keen interest lately in web development, and recently I've been building a few projects in React/Next.js. I love to learn, and ever since I finished my degree, that urge has been fueled like never before.
+                    I write code, mostly with TypeScript and Next.js, and I love building things that real humans use! I'd love to expand my skill set and learn from others in the industry. I've spent a while learning the "safe" stuff like React, Next.js, and TypeScript, but I'm eager to move outside of the box.
 
                     <br></br><br></br>
 
-                    Currently learning: <b>C#</b><b></b>.
+                    Currently building <a href="https://github.com/masonym/phace-website" target="_blank" rel="noopener noreferrer">Phace</a>, a full-stack booking and e-commerce website for a small business in the medical aesthetics industry, located in Chilliwack, British Columbia. Designed to replace a Wix website and acuity scheduling platform. Built to extend the limitations of off-the-shelf booking tools by building a fully customized frontend integrated with the Square API.
                 </p>
             </div>
         </div>
